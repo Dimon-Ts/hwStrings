a46dcff496f528f07e523612e2203da256328df9
Dimon-Ts <tselishev.dimon@yandex.ru>
2022-10-01 20:24:37 +0300

    Строки.Домашняя работа.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-
+        // Строки.Домашнее задание.
+        System.out.println("Строки.Домашнее задание.");
+        // Задание 1.
+        System.out.println("Задание 1.");
+        String firstName = " Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника - " + fullName + ".");
+        // Задание 2.
+        System.out.println("Задание 2.");
+        String fullNameBig = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameBig + ".");
+        // Задание 3.
+        System.out.println("Задание 3.");
+        String firstNameRu = " Семён ";
+        String middleNameRu = "Семёнович";
+        String lastNameRu = "Иванов";
+        String fullNameRu = lastNameRu + firstNameRu + middleNameRu;
+        fullNameRu = fullNameRu.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + fullNameRu + ".");
     }
 }
